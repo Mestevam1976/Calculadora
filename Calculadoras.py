7b94ece0076c38b6093de7161beedf0d1d5a4f60
Mestevam1976 <marcio.estevam@yahoo.com>
2021-11-17 14:52:27 -0300

    Função Resto adicionado na Calculadora

--- Calculadoras.py
+++ Calculadoras.py
@@ -2,7 +2,7 @@ import funcoes
 
 funcoes.clear()
 texto = ('='*30 + 'CALCULADORA EXPERIMENTAL'+'='*30)
-print(funcoes.cor('red',texto))
+print(funcoes.cor('red', texto))
 
 texto = '  Selecione uma das opções abaixo: '
 print(funcoes.cor('green', texto))
@@ -10,22 +10,35 @@ print('=' * 85)
 print('[A] Verifica se o número é primo ou não ')
 print('[B] Calcula o fatorial de um número inteiro ')
 print('[C] Cálculo das raízes Reais (Bhaskara) ')
+print('[D] Cálculo do resto da divisão')
 print('=' * 85)
+
 
 atende = False
 while atende == False:
-    seleciona_calculadora = input('Digite a letra correspondente a calculadora desejada: ')
+    seleciona_calculadora = input(
+        'Digite a letra correspondente a calculadora desejada: ').lower()
 
     if seleciona_calculadora == 'A' or seleciona_calculadora == 'a':
         funcoes.numero_Primo()
         atende = True
+
     elif seleciona_calculadora == 'B' or seleciona_calculadora == 'b':
         funcoes.fatorial()
         atende = True
+
     elif seleciona_calculadora == 'C' or seleciona_calculadora == 'c':
         funcoes.bhaskara()
         atende = True
+
+    elif seleciona_calculadora == 'd':
+        a = int(input('Digite valor a: '))
+        b = int(input('Digite valor b: '))
+
+        print(f"Resultado de {a} módulo {b} = {a%b}")
+        atende = True
+
     else:
         texto = ('Digite somente as letras do menu!!!')
-        print(funcoes.cor('yellow',texto))
+        print(funcoes.cor('yellow', texto))
         atende = False
